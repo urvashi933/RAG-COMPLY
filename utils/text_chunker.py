d1d9fe5324169e3b2395965a82c75c81b3c095f7
Urvashi <srivastavaurvashi933@gmail.com>
2026-04-09 14:12:09 +0530

    m commit

--- utils/text_chunker.py
+++ utils/text_chunker.py
@@ -1,3 +1,9 @@
-def chunk_text(text, size=600):
-	words = text.split()
-	return [" ".join(words[i:i+size]) for i in range(0, len(words), size)]
+def chunk_text(text, chunk_size=500):
+    words = text.split()
+    chunks = []
+
+    for i in range(0, len(words), chunk_size):
+        chunk = " ".join(words[i:i + chunk_size])
+        chunks.append(chunk)
+
+    return chunks
