1dd6d545d4fcda605dbd7dcfe23826de6b1db0c4
Urvashi <srivastavaurvashi933@gmail.com>
2026-04-24 13:09:11 +0530

    commit

--- test_rag.py
+++ test_rag.py
@@ -1,6 +1,6 @@
 from utils.rag_pipeline import rag_answer
 
-query = "who is dev ?" 
+query = "What are the legal requirements for the composition of an Internal Committee (IC), and why must at least one member be an external person from an NGO or an association committed to the cause of women?" 
 sector = "workforce"
 result = rag_answer(query, sector)
 
